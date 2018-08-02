04ba21d15c27bd914f9869fae2a43bbd8f83578b
Isaac <isaaclubanko@gmail.com>
2018-08-02 19:39:14 -0400

    can now select from a menu how it calculates the hours and minutes

---
+++ hours.py
@@ -0,0 +1,19 @@
+def user():
+    
+     userhours = [
+                #Insert your Data here
+                #It MUST be formatted as "hh:mm", with quotations and seperated by a comma
+                #Example: 22 hours and 3 minutes would be "22:03",
+                "00:15",
+                "02:17",
+                "00:31",
+                "00:25",
+                "30:45",
+                "00:14",
+                "04:17",
+                "00:32",
+                "05:15",
+                "00:15",
+                "20:30",            
+        ];
+     return userhours;
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
+import hours;
 
-
-worktime = [
-    "00:15",
-    "02:17",
-    "00:31",
-    "00:25",
-    "30:45",
-    "00:14",
-    "04:17",
-    "00:32",
-    "05:15",
-    "00:15",
-    "20:30",
-]
+worktime = []
 currentHours = 0;
 currentMinutes = 0;
 totalHours = 0;
@@ -21,30 +9,54 @@ totalMinutes = 0;
 def printTime(hours, minutes):
     print(str(hours) + " hours and " + str(minutes) + " minutes");
 
-def readFromArray():
+def readFromModule():
+    worktime = hours.user();
+    readFromArray(worktime);
+
+def readFromArray(worktime):
     global currentHours;
     global currentMinutes;
     global totalHours;
     global totalMinutes;
     for i in worktime:
         a = i.split(":");
-
         currentHours = int(a[0]);
         currentMinutes = int(a[1]);
         totalHours += int(currentHours);
         totalMinutes += int(currentMinutes);
+        printTime(currentHours, currentMinutes);
+    print("------------------------------------------");
+    print("TOTAL:");
+
+def calculateTotal:
+    global totalHours;
+    global totalMinutes;
     totalHours += int(totalMinutes / 60);
     totalMinutes = totalMinutes % 60;
     printTime(totalHours, totalMinutes);
-    
+
+
     
 
 def readFromInput():
+    print("user Input coming soon!");
     pass;
+
+def menu():
+    introText = "Would you like to: \n a)read from the file or \n b)read from user input?"
+    selection = input(introText + "\n->");
+    if selection == "a":
+        readFromModule();
+    elif selection == "b":
+        readFromInput();
+    else:
+        print("please type a valid answer!");
+        selection();
     
 
 if __name__ == "__main__":
-    readFromArray();
+    menu();
+
 
     
     
